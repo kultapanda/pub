2ab527ec11e6706e13c8fc156cc996945f6a86ab
Sean Kepner <skepner@ivytech.edu>
2020-02-20 08:35:03 -0500

    initial config repo bootstrap

---
+++ bootstrap.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+[[ command -v git ]] || sudo apt install -y git
+
+# Create repo dir and clone config repo
+repoDir="${HOME}/repos"
+configRepoUrl="https://github.com/kultapanda/private_no_input.git"
+[[ -d "${repoDir}" ]] || {
+  mkdir -p "${repoDir}"
+  cd "${repoDir}"
+  git clone "${configRepoUrl}"
+  }
+
+echo "run relink, setup, and local conf scripts"
